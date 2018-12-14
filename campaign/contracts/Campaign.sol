e8ebabca2fb86fd76bbaf1d31dc3aca8ea4acb90
Kinan Faham <k@theSilentCamera.com>
2018-12-14 17:13:10 -0500

    Add a campaign contract factory

--- campaign/contracts/Campaign.sol
+++ campaign/contracts/Campaign.sol
@@ -1,5 +1,19 @@
 pragma solidity >=0.4.22 <0.6.0;
 
+contract CampaignFactory {
+    // Enables a user to deploy a new campaign contract
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimumContribution) public {
+        address c = address(new Campaign(minimumContribution, msg.sender));
+        deployedCampaigns.push(c);
+    }
+    
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
 contract Campaign {
     // structs define new types
     struct Request {
@@ -17,13 +31,13 @@ contract Campaign {
     mapping(address => bool) public approvers;
     uint approversCount = 0;
 
-    constructor (uint minimumContr) public payable {
+    constructor (uint minimumContr, address creator) public payable {
         // the global msg object has the following properties
         // data: a dictionary of key, value pairs sent with the transaction invoking the function
         // gas: the available gas for the current function invocation
         // sender: the address of the account invoking the function
         // value: The amount of ether sent along with the function invocation
-        manager = msg.sender;
+        manager = creator;
         minimumContribution = minimumContr;
     }
     
