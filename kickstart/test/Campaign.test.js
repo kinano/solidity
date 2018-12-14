a7908e845fdd34f9e487c3b4df26dbd4b174440b
Kinan Faham <k@theSilentCamera.com>
2018-12-14 18:29:20 -0500

    Add a bunch of tests to the kickstart contract

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -41,4 +41,75 @@ describe('kickstart campaigns', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
     });
+
+    it('marks caller as campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('allows peeps to contribute and become approvers', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[1],
+            value: '200'
+        });
+        const isContributer = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributer);
+    });
+
+    it('Enforces minimum contribution rules', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                from: accounts[1],
+                value: '10'
+            });
+            assert(false)
+        }
+        catch (err) {
+            assert(err);
+        }
+    });
+
+    it('allows manager to request a payment', async () => {
+        await campaign.methods.createRequest(
+            'haha',
+            '100',
+            accounts[1]
+        ).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+        const request = await campaign.methods.requests(0).call();
+        assert.equal(request.description, 'haha');
+    });
+
+    it('processes requests', async () => {
+        const manager = accounts[0];
+        const approver = accounts[1];
+        const recipient = accounts[2];
+        await campaign.methods.contribute().send({
+            from: approver,
+            value: web3.utils.toWei('10', 'ether')
+        });
+        await campaign.methods.createRequest(
+            'a',
+            web3.utils.toWei('5', 'ether'),
+            recipient
+        ).send({
+            from: manager,
+            gas: 1000000
+        });
+        await campaign.methods.approveRequest(0).send({
+            from: approver,
+            gas: 1000000
+        });
+        await campaign.methods.finalizeRequest(0).send({
+            from: manager,
+            gas: '1000000'
+        });
+        let balance = await web3.eth.getBalance(recipient);
+        balance = web3.utils.fromWei(balance, 'ether');
+        balance = parseFloat(balance);
+        assert(balance === 105);
+    });
+
 });
