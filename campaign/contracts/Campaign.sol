d4a456f4cc7917bda4fa4260f727729b12aee14d
Kinan Faham <k@theSilentCamera.com>
2018-12-14 16:27:19 -0500

    Add a campaign solidity contract

---
+++ campaign/contracts/Campaign.sol
@@ -0,0 +1,69 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Campaign {
+    // structs define new types
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+
+    constructor (uint minimumContr) public payable {
+        // the global msg object has the following properties
+        // data: a dictionary of key, value pairs sent with the transaction invoking the function
+        // gas: the available gas for the current function invocation
+        // sender: the address of the account invoking the function
+        // value: The amount of ether sent along with the function invocation
+        manager = msg.sender;
+        minimumContribution = minimumContr;
+    }
+    
+    function contribute() public payable meetsMinContribution {
+        approvers[msg.sender] = true;
+    }
+    
+    modifier meetsMinContribution() {
+        require(msg.value >= minimumContribution);
+        _;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    // The request r cannot be stored in storage because there is no contract level property that refers to it directly
+    // For some reason, the solidity compiler makes a big deal of explicitly specifying memory as the holder for the request instance
+    function createRequest(string memory description, uint value, address recipient) public restricted {
+        Request memory r = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        requests.push(r);
+    }
+    
+    modifier isApprover() {
+        require(approvers[msg.sender]);
+        _;
+    }
+    
+    function approveRequest(uint index) public isApprover {
+        // Prevent duplicate votes
+        Request storage r = requests[index]; 
+        require(!r.approvals[msg.sender]);
+        r.approvals[msg.sender] = true;
+        r.approvalCount++;
+    }
+    
+}
