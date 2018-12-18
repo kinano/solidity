7aa7a3c24c70b270e5bb09428b73e72004634b1c
Kinan Faham <k@theSilentCamera.com>
2018-12-17 19:32:30 -0500

    Implement Campaign Details

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-
+import CampaignGetter from '../../ethereum/campaign';
+import { Card } from 'semantic-ui-react';
+import web3 from '../../ethereum/web3';
 class CampaignShow extends Component {
+
+    static async getInitialProps(props) {
+        const campaign = await CampaignGetter(props.query.address);
+        const summary = await campaign.methods.getSummary().call();
+        console.error(summary);
+        return {
+            minimumContribution: summary[0],
+            balance: web3.utils.fromWei(summary[1], 'ether'),
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        };
+    }
+
+    getDetails() {
+        const {
+            minimumContribution,
+            balance,
+            requestsCount,
+            approversCount,
+            manager
+        } = this.props;
+        const items = [
+            {
+                header: manager,
+                meta: 'Address of Manager',
+                description: 'Manager blah blah',
+                style: { overflowWrap: 'break-word' }
+            },
+            {
+                header: minimumContribution,
+                meta: 'Minimum Contribution (wei)',
+                description: "You must contribute at least this much wei"
+            },
+            {
+                header: balance,
+                meta: 'Balance in Ether'
+            },
+            {
+                header: requestsCount,
+                meta: 'Number of requests'
+            },
+            {
+                header: approversCount,
+                meta: 'Number of approvers'
+            }
+        ]
+        return (
+            <Card.Group items={items} />
+        );
+
+    }
+
     render() {
         return (
             <Layout>
-                <h3>SHOW MEEEE!</h3>
+                <h3>Campaign Details</h3>
+                {this.getDetails()}
             </Layout>
         );
     }
