35d555437dfdf7f73546ae52ce8011cfbc7cab7c
Kinan Faham <k@theSilentCamera.com>
2018-12-18 15:53:00 -0500

    Finalize the contribution form

--- kickstart/components/ContributionForm.js
+++ kickstart/components/ContributionForm.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import { Form, Input, Message, FormField, Button } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributionForm extends Component {
     state = {
         value: '',
-
+        loading: false,
+        error: ''
     };
 
     onSubmit = async (e) => {
         e.preventDefault();
+        this.setState({
+            loading: true
+        });
         const campaign = Campaign(this.props.address);
         try {
             const accounts = await web3.eth.getAccounts();
@@ -18,15 +23,24 @@ class ContributionForm extends Component {
                 from: accounts[0],
                 value: web3.utils.toWei(this.state.value, 'ether')
             });
+            Router.replaceRoute('/campaigns/`${this.props.address}`');
+            this.setState({ value: '' });
         }
         catch (err) {
-            console.error(err);
+            this.setState({
+                error: err.message
+            });
+        }
+        finally {
+            this.setState({
+                loading: false
+            });
         }
     };
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.error}>
                 <FormField>
                     <label>
                         Amount to contribute
@@ -38,7 +52,8 @@ class ContributionForm extends Component {
                         onChange={(e) => { this.setState({ value: e.target.value }) }}
                     />
                 </FormField>
-                <Button primary>
+                <Message error header="Oops" content={this.state.error} />
+                <Button primary loading={this.state.loading}>
                     Contribute
                 </Button>
             </Form>
