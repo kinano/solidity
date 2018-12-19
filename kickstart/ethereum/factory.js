99b429e6a9b9f388950fd163b84f4e8d71631abc
Kinan Faham <k@theSilentCamera.com>
2018-12-18 19:05:49 -0500

    Redeploy the contract

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -4,7 +4,7 @@ import CampaignFactory from './build/CampaignFactory';
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
     // @todo @kinano Move this to a config file
-    '0xeF2532820CC0a96BE4291B5FcE5247C11b8575E0'
+    '0x817F15e929Dd58e930510737B56ED376F4599899'
 );
 
 export default instance;
